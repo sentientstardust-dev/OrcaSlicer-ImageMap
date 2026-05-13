cf129f9ded7a984d39d473df3f8d3c04d4694801
sentientstardust <ssd@gradients.garden>
2026-05-13 04:41:33 +0100

    Stop commit hash being a global compiler definition to reduce build time

--- cmake/GenerateSoftFeverVersion.cmake
+++ cmake/GenerateSoftFeverVersion.cmake
@@ -6,6 +6,10 @@ if(NOT DEFINED OUTPUT)
     message(FATAL_ERROR "OUTPUT is required")
 endif()
 
+if(NOT DEFINED GIT_COMMIT_HASH)
+    set(GIT_COMMIT_HASH "0000000")
+endif()
+
 file(STRINGS "${INPUT}" SOFTFEVER_VERSION LIMIT_COUNT 1)
 string(STRIP "${SOFTFEVER_VERSION}" SOFTFEVER_VERSION)
 
@@ -17,6 +21,11 @@ namespace Slic3r {
 const char* softfever_version()
 {
     return "@SOFTFEVER_VERSION@";
+}
+
+const char* git_commit_hash()
+{
+    return "@GIT_COMMIT_HASH@";
 }
 
 } // namespace Slic3r
